6292c905a8a85011815b1b3af1ec0f9cf1a5b817
Oleh Skrypets <olehskrupets@gmail.com>
2026-03-01 20:02:02 +0200

    add img catalog

--- src/js/catalog-data.js
+++ src/js/catalog-data.js
@@ -1,15 +1,326 @@
 export const catalogData = {
-  branches: [{ img: "/img/products/branches/1.webp", price: 1000 }],
+  branches: [
+    {
+      img: "/img/products/branches/1.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/branches/2.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/branches/3.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/branches/4.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/branches/5.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/branches/6.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/branches/7.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/branches/8.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/branches/9.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/branches/10.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/branches/11.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/branches/12.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/branches/13.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/branches/14.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/branches/15.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/branches/16.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/branches/17.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/branches/18.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/branches/20.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/branches/19.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/branches/21.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/branches/22.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/branches/23.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/branches/24.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/branches/25.webp",
+      price: 1000,
+    },
+  ],
 
-  headbands: [{ img: "/img/products/headbands/1.webp", price: 1000 }],
+  headbands: [
+    {
+      img: "/img/products/headbands/1.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/headbands/2.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/headbands/3.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/headbands/4.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/headbands/5.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/headbands/6.webp",
+      price: 1000,
+    },
+  ],
 
-  earrings: [{ img: "/img/products/earrings/1.webp", price: 1000 }],
+  earrings: [
+    {
+      img: "/img/products/earrings/1.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/earrings/2.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/earrings/3.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/earrings/4.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/earrings/5.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/earrings/6.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/earrings/7.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/earrings/8.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/earrings/9.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/earrings/10.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/earrings/11.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/earrings/12.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/earrings/13.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/earrings/14.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/earrings/15.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/earrings/16.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/earrings/17.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/earrings/18.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/earrings/19.webp",
+      price: 1000,
+    },
+  ],
 
-  necklaces: [{ img: "/img/products/necklaces/1.webp", price: 1000 }],
+  necklaces: [
+    {
+      img: "/img/products/necklaces/1.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/necklaces/2.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/necklaces/3.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/necklaces/4.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/necklaces/5.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/necklaces/6.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/necklaces/7.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/necklaces/8.webp",
+      price: 1000,
+    },
+  ],
 
-  pins: [{ img: "/img/products/pins/1.webp", price: 1000 }],
+  pins: [
+    {
+      img: "/img/products/pins/1.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/pins/2.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/pins/3.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/pins/4.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/pins/5.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/pins/6.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/pins/7.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/pins/8.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/pins/9.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/pins/10.webp",
+      price: 1000,
+    },
+  ],
 
-  rings: [{ img: "/img/products/rings/1.webp", price: 1000 }],
+  rings: [
+    {
+      img: "/img/products/rings/1.webp",
+      price: 1000,
+    },
+  ],
 
-  combs: [{ img: "/img/products/combs/1.webp", price: 1000 }],
+  combs: [
+    {
+      img: "/img/products/combs/1.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/combs/2.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/combs/3.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/combs/4.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/combs/5.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/combs/6.webp",
+      price: 1000,
+    },
+    {
+      img: "/img/products/combs/7.webp",
+      price: 1000,
+    },
+  ],
 };
