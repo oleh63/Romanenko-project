88cf3ad51ac3c5e67cc3ea1676f76d9810da2ae3
Oleh Skrypets <olehskrupets@gmail.com>
2026-03-05 20:28:32 +0200

    fix mob-menu

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -5,11 +5,17 @@ const mobMenu = document.querySelector(".mob-menu");
 function openMenu() {
   mobMenu.classList.add("is-open");
   document.body.classList.add("no-scroll");
+
+  history.pushState({ menu: true }, "");
 }
 
 function closeMenu() {
   mobMenu.classList.remove("is-open");
   document.body.classList.remove("no-scroll");
+
+  if (history.state?.menu) {
+    history.back();
+  }
 }
 
 openBtn.addEventListener("click", openMenu);
@@ -26,5 +32,12 @@ mobMenu.addEventListener("click", (e) => {
 mobMenu.addEventListener("click", (e) => {
   if (e.target.closest("a")) {
     closeMenu();
+  }
+});
+
+window.addEventListener("popstate", () => {
+  if (mobMenu.classList.contains("is-open")) {
+    mobMenu.classList.remove("is-open");
+    document.body.classList.remove("no-scroll");
   }
 });
