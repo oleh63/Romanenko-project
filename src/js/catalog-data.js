0a06b12dbedb0e1b5dc586670040c8a1fa32f941
Oleh Skrypets <olehskrupets@gmail.com>
2026-03-06 18:49:32 +0200

    add price catalog

--- src/js/catalog-data.js
+++ src/js/catalog-data.js
@@ -2,127 +2,122 @@ export const catalogData = {
   branches: [
     {
       img: "/img/products/branches/1.webp",
-      price: 1000,
+      price: 1570,
       alt: "гілочки",
     },
     {
       img: "/img/products/branches/2.webp",
-      price: 1000,
+      price: 830,
       alt: "гілочки",
     },
     {
       img: "/img/products/branches/3.webp",
-      price: 1000,
+      price: 670,
       alt: "гілочки",
     },
     {
       img: "/img/products/branches/4.webp",
-      price: 1000,
+      price: 720, // 1 штука
       alt: "гілочки",
     },
     {
       img: "/img/products/branches/5.webp",
-      price: 1000,
+      price: 540,
       alt: "гілочки",
     },
     {
       img: "/img/products/branches/6.webp",
-      price: 1000,
+      price: 500,
       alt: "гілочки",
     },
     {
       img: "/img/products/branches/7.webp",
-      price: 1000,
+      price: 530,
       alt: "гілочки",
     },
     {
       img: "/img/products/branches/8.webp",
-      price: 1000,
+      price: 950,
       alt: "гілочки",
     },
     {
       img: "/img/products/branches/9.webp",
-      price: 1000,
+      price: 650,
       alt: "гілочки",
     },
     {
       img: "/img/products/branches/10.webp",
-      price: 1000,
+      price: 690,
       alt: "гілочки",
     },
     {
       img: "/img/products/branches/11.webp",
-      price: 1000,
+      price: 880,
       alt: "гілочки",
     },
     {
       img: "/img/products/branches/12.webp",
-      price: 1000,
+      price: 820,
       alt: "гілочки",
     },
     {
       img: "/img/products/branches/13.webp",
-      price: 1000,
-      alt: "гілочки",
-    },
-    {
-      img: "/img/products/branches/14.webp",
-      price: 1000,
-      alt: "гілочки",
-    },
-    {
-      img: "/img/products/branches/15.webp",
-      price: 1000,
-      alt: "гілочки",
-    },
-    {
-      img: "/img/products/branches/16.webp",
-      price: 1000,
-      alt: "гілочки",
-    },
-    {
-      img: "/img/products/branches/17.webp",
-      price: 1000,
-      alt: "гілочки",
-    },
-    {
-      img: "/img/products/branches/18.webp",
-      price: 1000,
-      alt: "гілочки",
-    },
-    {
-      img: "/img/products/branches/20.webp",
-      price: 1000,
-      alt: "гілочки",
-    },
-    {
-      img: "/img/products/branches/19.webp",
-      price: 1000,
-      alt: "гілочки",
-    },
-    {
-      img: "/img/products/branches/21.webp",
-      price: 1000,
-      alt: "гілочки",
-    },
-    {
-      img: "/img/products/branches/22.webp",
-      price: 1000,
-      alt: "гілочки",
-    },
-    {
-      img: "/img/products/branches/23.webp",
-      price: 1000,
-      alt: "гілочки",
-    },
-    {
-      img: "/img/products/branches/24.webp",
-      price: 1000,
+      price: 730,
       alt: "гілочки",
     },
     {
       img: "/img/products/branches/25.webp",
-      price: 1000,
+      price: 730,
+      alt: "гілочки",
+    },
+    {
+      img: "/img/products/branches/15.webp",
+      price: 880,
+      alt: "гілочки",
+    },
+    {
+      img: "/img/products/branches/16.webp",
+      price: 720,
+      alt: "гілочки",
+    },
+    {
+      img: "/img/products/branches/17.webp",
+      price: 680,
+      alt: "гілочки",
+    },
+    {
+      img: "/img/products/branches/18.webp",
+      price: 840,
+      alt: "гілочки",
+    },
+    {
+      img: "/img/products/branches/20.webp",
+      price: 1150,
+      alt: "гілочки",
+    },
+    {
+      img: "/img/products/branches/19.webp",
+      price: 1100,
+      alt: "гілочки",
+    },
+    {
+      img: "/img/products/branches/21.webp",
+      price: 700,
+      alt: "гілочки",
+    },
+    {
+      img: "/img/products/branches/22.webp",
+      price: 680,
+      alt: "гілочки",
+    },
+    {
+      img: "/img/products/branches/23.webp",
+      price: 730,
+      alt: "гілочки",
+    },
+    {
+      img: "/img/products/branches/24.webp",
+      price: 720,
       alt: "гілочки",
     },
   ],
@@ -130,32 +125,32 @@ export const catalogData = {
   headbands: [
     {
       img: "/img/products/headbands/1.webp",
-      price: 1000,
+      price: 650,
       alt: "обручі",
     },
     {
       img: "/img/products/headbands/2.webp",
-      price: 1000,
+      price: 1600,
       alt: "обручі",
     },
     {
       img: "/img/products/headbands/3.webp",
-      price: 1000,
+      price: 650,
       alt: "обручі",
     },
     {
       img: "/img/products/headbands/4.webp",
-      price: 1000,
+      price: 1300,
       alt: "обручі",
     },
     {
       img: "/img/products/headbands/5.webp",
-      price: 1000,
+      price: 1250,
       alt: "обручі",
     },
     {
       img: "/img/products/headbands/6.webp",
-      price: 1000,
+      price: 1500,
       alt: "обручі",
     },
   ],
@@ -163,140 +158,125 @@ export const catalogData = {
   earrings: [
     {
       img: "/img/products/earrings/1.webp",
-      price: 1000,
+      price: 950,
       alt: "сережки",
     },
     {
       img: "/img/products/earrings/2.webp",
-      price: 1000,
+      price: 750,
       alt: "сережки",
     },
     {
       img: "/img/products/earrings/3.webp",
-      price: 1000,
+      price: 950,
       alt: "сережки",
     },
     {
       img: "/img/products/earrings/4.webp",
-      price: 1000,
+      price: 550,
       alt: "сережки",
     },
     {
       img: "/img/products/earrings/5.webp",
-      price: 1000,
+      price: 850,
       alt: "сережки",
     },
     {
       img: "/img/products/earrings/6.webp",
-      price: 1000,
-      alt: "сережки",
-    },
-    {
-      img: "/img/products/earrings/7.webp",
-      price: 1000,
+      price: 680,
       alt: "сережки",
     },
     {
       img: "/img/products/earrings/8.webp",
-      price: 1000,
+      price: 790,
       alt: "сережки",
     },
     {
       img: "/img/products/earrings/9.webp",
-      price: 1000,
+      price: 780,
       alt: "сережки",
     },
     {
       img: "/img/products/earrings/10.webp",
-      price: 1000,
+      price: 850,
       alt: "сережки",
     },
     {
       img: "/img/products/earrings/11.webp",
-      price: 1000,
+      price: 950,
       alt: "сережки",
     },
     {
       img: "/img/products/earrings/12.webp",
-      price: 1000,
+      price: 670,
       alt: "сережки",
     },
     {
       img: "/img/products/earrings/13.webp",
-      price: 1000,
-      alt: "сережки",
-    },
-    {
-      img: "/img/products/earrings/14.webp",
-      price: 1000,
+      price: 700,
       alt: "сережки",
     },
     {
       img: "/img/products/earrings/15.webp",
-      price: 1000,
+      price: 760,
       alt: "сережки",
     },
     {
       img: "/img/products/earrings/16.webp",
-      price: 1000,
+      price: 550,
       alt: "сережки",
     },
     {
       img: "/img/products/earrings/17.webp",
-      price: 1000,
+      price: 650,
       alt: "сережки",
     },
     {
       img: "/img/products/earrings/18.webp",
-      price: 1000,
+      price: 820,
       alt: "сережки",
     },
     {
       img: "/img/products/earrings/19.webp",
-      price: 1000,
+      price: 500,
       alt: "сережки",
     },
   ],
 
   necklaces: [
     {
-      img: "/img/products/necklaces/1.webp",
-      price: 1000,
-      alt: "намиста",
-    },
-    {
       img: "/img/products/necklaces/2.webp",
-      price: 1000,
+      price: 650,
       alt: "намиста",
     },
     {
       img: "/img/products/necklaces/3.webp",
-      price: 1000,
+      price: 690,
       alt: "намиста",
     },
     {
       img: "/img/products/necklaces/4.webp",
-      price: 1000,
+      price: 680,
       alt: "намиста",
     },
     {
       img: "/img/products/necklaces/5.webp",
-      price: 1000,
+      price: 670,
       alt: "намиста",
     },
     {
       img: "/img/products/necklaces/6.webp",
-      price: 1000,
+      price: 850,
       alt: "намиста",
     },
     {
       img: "/img/products/necklaces/7.webp",
-      price: 1000,
+      price: 690,
       alt: "намиста",
     },
     {
       img: "/img/products/necklaces/8.webp",
-      price: 1000,
+      price: 650,
       alt: "намиста",
     },
   ],
@@ -304,85 +284,83 @@ export const catalogData = {
   pins: [
     {
       img: "/img/products/pins/1.webp",
-      price: 1000,
+      price: 250, // ціна за штуку
       alt: "шпильки",
     },
     {
       img: "/img/products/pins/2.webp",
-      price: 1000,
+      price: 640,
       alt: "шпильки",
     },
     {
       img: "/img/products/pins/3.webp",
-      price: 1000,
+      price: 250, // ціна за штуку
       alt: "шпильки",
     },
     {
       img: "/img/products/pins/4.webp",
-      price: 1000,
+      price: 650, // ціна за 3 штуки
       alt: "шпильки",
     },
     {
       img: "/img/products/pins/5.webp",
-      price: 1000,
+      price: 550,
       alt: "шпильки",
     },
     {
       img: "/img/products/pins/6.webp",
-      price: 1000,
+      price: 720,
       alt: "шпильки",
     },
     {
       img: "/img/products/pins/7.webp",
-      price: 1000,
+      price: 650, // ціна за 3 штуки
       alt: "шпильки",
     },
     {
       img: "/img/products/pins/8.webp",
-      price: 1000,
+      price: 420,
       alt: "шпильки",
     },
     {
       img: "/img/products/pins/9.webp",
-      price: 1000,
+      price: 600, // ціна за 3 штуки
       alt: "шпильки",
     },
-    {
-      img: "/img/products/pins/10.webp",
-      price: 1000,
-      alt: "шпильки",
-    },
+
+    // видалити
+
+    // {
+    //   img: "/img/products/pins/10.webp",
+    //   price: 1000,
+    //   alt: "шпильки",
+    // },
   ],
 
   rings: [
     {
       img: "/img/products/rings/1.webp",
-      price: 1000,
+      price: 500,
       alt: "каблучки",
     },
     {
       img: "/img/products/rings/2.webp",
-      price: 1000,
+      price: 480,
       alt: "каблучки",
     },
     {
       img: "/img/products/rings/3.webp",
-      price: 1000,
-      alt: "каблучки",
-    },
-    {
-      img: "/img/products/rings/4.webp",
-      price: 1000,
+      price: 480,
       alt: "каблучки",
     },
     {
       img: "/img/products/rings/5.webp",
-      price: 1000,
+      price: 450,
       alt: "каблучки",
     },
     {
       img: "/img/products/rings/6.webp",
-      price: 1000,
+      price: 500,
       alt: "каблучки",
     },
   ],
@@ -390,37 +368,37 @@ export const catalogData = {
   combs: [
     {
       img: "/img/products/combs/1.webp",
-      price: 1000,
+      price: 850,
       alt: "гребінці",
     },
     {
       img: "/img/products/combs/2.webp",
-      price: 1000,
+      price: 750,
       alt: "гребінці",
     },
     {
       img: "/img/products/combs/3.webp",
-      price: 1000,
+      price: 600,
       alt: "гребінці",
     },
     {
       img: "/img/products/combs/4.webp",
-      price: 1000,
+      price: 640,
       alt: "гребінці",
     },
     {
       img: "/img/products/combs/5.webp",
-      price: 1000,
+      price: 550,
       alt: "гребінці",
     },
     {
       img: "/img/products/combs/6.webp",
-      price: 1000,
+      price: 1500, // size 22см
       alt: "гребінці",
     },
     {
       img: "/img/products/combs/7.webp",
-      price: 1000,
+      price: 850,
       alt: "гребінці",
     },
   ],
